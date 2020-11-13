1352d6dfa39d73dc4b57a4ec71e18c18da29507a
Naren B <naren.bandekar@mindcraft.in>
2020-11-13 13:23:53 +0530

    Add project files.

---
+++ EFTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EFTest/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFTest.Models
+{
+    public class User
+    {
+        [Key]
+        public int UerId { get; set; }
+        public string UserName { get; set; }
+    }
+}
